4385b62d0dbdfeb043d371fb9065a84a5ed5186b
Enrique <enriquealbuera60@gmail.com>
2024-11-12 13:39:11 +0100

    logo como boton

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const cloud = document.getElementById("cloud");
 const barraLateral=document.querySelector(".barra-lateral");
 const spans=document.querySelectorAll("span");//selecionamos todos los span
 const main =document.querySelector("main");
+const Original=document.querySelector(".Original");
+const mini=document.querySelector(".mini");
 
 cloud.addEventListener("click",()=>{
     barraLateral.classList.toggle("mini-barra-lateral"); //al hacer clic sustituiremos el formato normal de barra lateral por el de mini barra lateral y lo mismo al reves
@@ -9,5 +11,14 @@ cloud.addEventListener("click",()=>{
     spans.forEach((span)=>{//Recorremos todos los span que hay
         span.classList.toggle("oculto");
     });
+    Original.classList.toggle("esconder")
 });
 
+mini.addEventListener("click", () => {
+    barraLateral.classList.toggle("mini-barra-lateral"); //al hacer clic sustituiremos el formato normal de barra lateral por el de mini barra lateral y lo mismo al reves
+    main.classList.toggle("minmain");
+    spans.forEach((span)=>{//Recorremos todos los span que hay
+        span.classList.toggle("oculto");
+    });
+    Original.classList.toggle("esconder"); // Alterna la clase 'esconder' en .Original
+});

316a947c5712f7e630cd7c93837864c643bde2ef
Enrique <enriquealbuera60@gmail.com>
2024-11-21 14:15:31 +0100

    panaderias

--- xdev.js
+++ xdev.js
@@ -10,3 +10,5 @@ prev.addEventListener('click', function(){
     let items = document.querySelectorAll('.item')
     document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
 })
+
+
